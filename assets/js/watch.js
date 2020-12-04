4b831f607cc4980ce9894d0e1caed8bd26462bcc
MalC-U <mwcalow@gmail.com>
2020-12-04 19:15:22 +0000

    Added function stopping and resseting stopwatch

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -3,19 +3,34 @@ const timerDisplay = document.getElementById("timer");
 const timerTwoDisplay = document.getElementById("timertwo");
 let timer;
 let timePassed = 0;
+let timerOn = false;
 
 //Function counting time in seconds only
 function timeCount() {
-    timePassed  += 1;
+    timePassed  += 1;   
     timerDisplay.innerHTML = timePassed + " seconds";    
 };
 
 //Function starting second counting
 function timeStart() {
-if (!timerOn) {
+    if (!timerOn) {
     timerOn = true;
     timer = setInterval(timeCount, 1000);
+    }
+};
+
+//Function stopping second counting
+function timeStop() {
+    clearInterval(timer);
+    timerOn = false;
+
 }
 
-};
+//Function resetting second counting
+function timeReset() {
+    clearInterval(timer);
+    timerOn = false;
+    timePassed = 0;
+    timerDisplay.innerHTML = timePassed + " seconds";    
+}
 
